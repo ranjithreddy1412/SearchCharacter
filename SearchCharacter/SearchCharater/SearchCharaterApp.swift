42fda9fdced3132adc4a2b2d23775acb05f01a85
ranjith <ranjith.perkam14@gmail.com>
2025-04-01 15:35:30 -0500

    initial commit

---
+++ SearchCharacter/SearchCharater/SearchCharaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchCharaterApp.swift
+//  SearchCharater
+//
+//  Created by ranjith kumar reddy b perkampally on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SearchCharaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
